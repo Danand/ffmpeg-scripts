e67634954331e77d3e7a5a4c0feb264c5fc6519b
Andrey Danilov <danand@inbox.ru>
2023-05-05 16:49:39 +0300

    Add trim script for editor

---
+++ editor/trim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Trims video of given input video path using given frame numbers: from and to.
+# Returns trimmed output video.
+
+set -e
+
+source "../pipeline/utils.sh"
+
+input_path="$(cat)"
+input_name="$(echo "${input_path}" | basename "$(cat)" | strip_extension "$(cat)")"
+
+frame_from="$1"
+frame_to="$2"
+
+output_path="./outputs/${input_name}-${frame_from}-${frame_to}.mp4"
+
+ffmpeg \
+  -i "${input_path}" \
+  -filter:v "trim=start_frame=${frame_from}:end_frame=${frame_to},setpts=PTS-STARTPTS" \
+  -c:a "copy" \
+  "${output_path}"
+
+echo "${output_path}"
