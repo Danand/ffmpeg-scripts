e640059dd793c4965ae655b89896429d81ea8cc4
Andrey Danilov <danand@inbox.ru>
2023-05-06 07:44:49 +0300

    Introduce separate script for rendering subs without overriding style

--- pipeline/add-subs-retro.sh
+++ pipeline/add-subs-retro.sh
@@ -15,17 +15,18 @@ subtitles_name="$(echo "${subtitles_path}" | basename "$(cat)" | strip_extension
 
 output_path="./outputs/${input_name}-${subtitles_name}.mp4"
 
-subtitles_filter="subtitles"
-
-if echo "${subtitles_path}" | grep -q "\.ass$"; then
-  subtitles_filter="ass"
-fi
-
-video_filter="subtitles=${subtitles_path}"
+video_filter="\
+  subtitles=${subtitles_path} \
+    :force_style='\
+      Fontname=Arial, \
+      Fontsize=16, \
+      PrimaryColour=&H03fcff, \
+      Italic=1'"
 
 ffmpeg \
   -i "${input_path}" \
   -vf "${video_filter}" \
+  -c:a "copy" \
   "${output_path}"
 
 echo "${output_path}"
